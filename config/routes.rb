98c4f6d8135652ad7263826c2c4225d8c5730460
root <info@my-infotech.online>
2020-12-12 23:26:18 +0530

    0.2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  get 'home/cloud'
+  get 'home/about'
+  get 'home/contact'
+  get 'home/training'
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+root to: "home#index"
+
+
 end
